8a6ac1d0d87cb2b55a4342ce56a7392b652881aa
Winder Roman <winderroman3131@gmail.com>
2023-08-16 16:33:16 +0100

    agregando traductor

--- app-ai/assets/js/main.js
+++ app-ai/assets/js/main.js
@@ -70,14 +70,62 @@ function getAnality(img) {
         .catch(error => console.error('Error:', error));
 }
 
-const getUser = async () => {
-    try {
-        const data = await fetch("https://jsonplaceholder.typicode.com/users")
-        const resultado = await data.json();
-        resultado.forEach(element => {
-            console.log(element.name);
-        });
-    } catch (error) {
-        console.error(error);
+// const getUser = async () => {
+//     try {
+//         const data = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const resultado = await data.json();
+//         resultado.forEach(element => {
+//             console.log(element.name);
+//         });
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+
+const text = document.getElementById('input');
+const resultado = document.getElementById('result');
+
+text.addEventListener('input',() =>{
+    console.log("traduciendo.....");
+    let texto = text.value;
+    if (text.value == '') {
+        console.log("ingresa lo que deseas traducir");
+        resultado.innerHTML = '';
+    }else{
+        traslator(texto);
     }
+})
+
+
+function traslator(texto) {
+    const key = '45256b4902f24462948fda7582f21d05';
+    const location = 'eastus';
+    const endpoint = 'https://api.cognitive.microsofttranslator.com/';
+
+    const text = texto;
+
+    const headers = {
+        "Ocp-Apim-Subscription-Key" : key,
+        "Ocp-Apim-Subscription-Region" : location,
+        "Content-Type" : "application/json"
+    };
+
+    const body = JSON.stringify([{
+        'text' : text
+    }]);
+
+    fetch(`${endpoint}translate?api-version=3.0&from=es&to=fr&to=en&to=zh-Hans`,{
+        method : 'POST',
+        headers : headers,
+        body : body
+    })
+    .then( response => response.json())
+    .then( result => {
+        console.log(result[0].translations[1].text);
+        resultado.innerHTML = result[0].translations[1].text
+    })
+    .catch( err => console.log(err));
+
 }
+
